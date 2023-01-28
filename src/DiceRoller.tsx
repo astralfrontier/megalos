97fdcbccfff94ad4a9e3932c567a498b68c59511
Bill Garrett <garrett@astralfrontier.org>
2023-01-28 14:36:30 -0800

    Add support for presets

--- src/DiceRoller.tsx
+++ src/DiceRoller.tsx
@@ -1,3 +1,4 @@
+// import localforage from 'localforage';
 import React, { useEffect } from 'react';
 import { useState } from 'react'
 
@@ -5,6 +6,13 @@ import DiceShelf from './DiceShelf';
 import FormInput from './FormInput';
 
 interface DiceRollerProps {
+}
+
+interface Preset {
+    label: string;
+    diceCount: number;
+    difficulty: number;
+    resistance: number;
 }
 
 function rollDie(min: number, max: number): number {
@@ -26,11 +34,24 @@ function DiceRoller(props: DiceRollerProps) {
     const [resistance, setResistance] = useState<number>(1)
     const [rerolls, setRerolls] = useState<number>(0)
     const [summary, setSummary] = useState<string>("")
+    const [presets, setPresets] = useState<Preset[]>([])
 
     function rollAndShow() {
         setRerolls(0)
         setRolls(rollDice(diceCount > 0 ? diceCount : 2))
     }
+
+    // useEffect(() => {
+    //     try {
+    //         localforage.ready().then(() => {
+    //             localforage.getItem("presets").then(value => {
+    //                 setPresets(value as Preset[] || [])
+    //             })
+    //         })    
+    //     } catch (err) {
+    //         alert("Error fetching presets")
+    //     }
+    // }, [])
 
     // Summarize the results
     useEffect(() => {
@@ -78,6 +99,33 @@ function DiceRoller(props: DiceRollerProps) {
         setRerolls(0)
     }
 
+    function savePreset() {
+        const presetName = prompt("Enter a name for this preset", "New Preset")
+        if (presetName != null) {
+            const newPresets = [...presets]
+            newPresets.push({
+                label: presetName,
+                diceCount: diceCount,
+                difficulty: difficulty,
+                resistance: resistance
+            })
+            setPresets(newPresets)
+        }
+    }
+
+    function usePreset(index: number) {
+        const preset = presets[index]
+        setDiceCount(preset.diceCount)
+        setDifficulty(preset.difficulty)
+        setResistance(preset.resistance)
+    }
+
+    function deletePreset(index: number) {
+        const newPresets = [...presets]
+        newPresets.splice(index, 1)
+        setPresets(newPresets)
+    }
+
     return (
         <>
             <DiceShelf rolls={rolls} onClick={(index: number) => {
@@ -86,8 +134,20 @@ function DiceRoller(props: DiceRollerProps) {
                 setRolls(newRolls)
                 setRerolls(rerolls + 1)
             }} />
+             <div className='box block'>
+                <p className='is-size-3'>{summary}</p>
+                <p>Click on a single die to reroll it.</p>
+            </div>
             <div className='box block'>
                 <div className='columns'>
+                <div className='column has-text-centered'>
+                        <button className='button is-primary mx-2' onClick={() => rollAndShow()}>Roll</button>
+                        <button className='button is-warning mx-2' onClick={() => resetRolls()}>Reset</button>
+                        <br />
+                        Rerolls Used: {rerolls}
+                        <br />
+                        <button className='button is-info' onClick={() => savePreset()}>Save</button>
+                    </div>
                     <div className='column'>
                         <FormInput label={'Dice'} min={0} max={8} getter={diceCount} setter={setDiceCount}>
                             Your skill rating, or the dice rating of your weapon, plus Advantage, minus Disadvantage
@@ -103,17 +163,23 @@ function DiceRoller(props: DiceRollerProps) {
                             The default value is 1, +1 each for magickal effects, Stressed Out, unfavorable conditions, and GM ruling
                         </FormInput>
                     </div>
-                    <div className='column has-text-centered'>
-                        <button className='button is-primary mx-2' onClick={() => rollAndShow()}>Roll</button>
-                        <button className='button is-warning mx-2' onClick={() => resetRolls()}>Reset</button>
-                        <br />
-                        Rerolls Used: {rerolls}
-                    </div>
                 </div>
             </div>
             <div className='box block'>
-                <p className='is-size-3'>{summary}</p>
-                <p>Click on a single die to reroll it.</p>
+                {presets.map((preset, index) => (
+                    <div className='columns is-multiline is-vcentered'>
+                        <div className='column is-narrow'>
+                            <button className='button is-primary' onClick={() => usePreset(index)}>Use</button>
+                        </div>
+                        <div className='column is-narrow'>
+                            <button className='button is-warning' onClick={() => deletePreset(index)}>Delete</button>
+                        </div>
+                        <div className='column'>
+                            {preset.label} ({preset.difficulty}/{preset.resistance}, {preset.diceCount} dice)
+                        </div>
+                    </div>
+                ))}
+                {presets.length ? <></> : <p className='subtitle'>No presets (click "Save" to create one)</p>}
             </div>
         </>
     )
