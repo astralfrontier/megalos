d3fd201b00d1d00e47c87075844272427f2ab905
Bill Garrett <garrett@astralfrontier.org>
2023-01-29 10:19:11 -0800

    Make navbar menu appear on mobile

--- src/AppNavbar.tsx
+++ src/AppNavbar.tsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-export const DICE_PATH = "/"
-export const LOADOUTS_PATH = "/loadouts"
+export const DICE_PATH = "/";
+export const LOADOUTS_PATH = "/loadouts";
 
-const activeNavbarItem = ({ isActive }) => isActive ? "navbar-item has-background-white" : "navbar-item"
+const activeNavbarItem = ({ isActive }) =>
+  isActive ? "navbar-item has-background-white" : "navbar-item";
 
 function AppNavbar() {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
   return (
-    <nav className="navbar block has-background-primary" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <a className="navbar-item">
-                MEGALOS
-            </a>
+    <nav
+      className="navbar block has-background-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item">MEGALOS</a>
+        <a
+          role="button"
+          className={isActive ? "navbar-burger is-active" : "navbar-burger"}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="appNavbarItems"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div className={isActive ? "navbar-menu is-active" : "navbar-menu"} id="appNavbarItems">
+        <div className="navbar-start">
+          <NavLink className={activeNavbarItem} to={DICE_PATH}>
+            Dice
+          </NavLink>
+          <NavLink className={activeNavbarItem} to={LOADOUTS_PATH}>
+            Loadouts
+          </NavLink>
         </div>
-        <div className="navbar-menu">
-            <div className="navbar-start">
-                <NavLink className={activeNavbarItem} to={DICE_PATH}>
-                    Dice
-                </NavLink>
-                <NavLink className={activeNavbarItem} to={LOADOUTS_PATH}>
-                    Loadouts
-                </NavLink>
-            </div>
-        </div>
+      </div>
     </nav>
   );
 }
