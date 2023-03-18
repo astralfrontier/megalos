852e51ffa2794d1fa5bd4f6a1b98d264ab1040d5
Bill Garrett <garrett@astralfrontier.org>
2023-03-18 04:39:50 -0700

    Redo Powers chooser to use checkboxes

--- src/characters/PowersTalentsChooser.tsx
+++ src/characters/PowersTalentsChooser.tsx
@@ -1,34 +1,10 @@
-import { add, allPass, append, assoc, filter, includes, intersection, map, prop, reduce, reject, remove, without } from 'ramda'
-import React, { useContext, useState } from 'react'
+import { add, append, assoc, filter, find, includes, map, propEq, reduce, reject, without } from 'ramda'
+import React, { useContext } from 'react'
+import slugify from 'slugify'
 
 import { CharacterContext } from '../GameStateProvider'
-import { meetsPrerequisites, MegalosCharacter, MegalosClassBenefits, MegalosPower, powers, recalculatePowers } from './data'
-import PowersTalentsPane, { PowerDisplay } from './PowersTalentsPane'
-
-interface TableRowProps {
-  power: MegalosPower
-  isSelected: boolean
-  togglePower: (power: MegalosPower) => any
-}
-
-function EditablePowerDisplay(props: TableRowProps) {
-  const { power, isSelected, togglePower } = props
-  return (
-    <PowerDisplay power={power}>
-      <div className='column is-narrow'>
-        <button className={isSelected ? 'button is-warning' : 'button is-primary'} onClick={() => togglePower(power)}>
-          <span className="icon">
-            <i className={isSelected ? "fa-solid fa-minus" : "fa-solid fa-plus"} />
-          </span>
-        </button>
-      </div>
-    </PowerDisplay>
-  )
-}
-
-interface BenefitsUsedProps {
-  character: MegalosCharacter
-}
+import { meetsPrerequisites, MegalosClassBenefits, MegalosPower, powers, recalculatePowers } from './data'
+import PowersTalentsPane from './PowersTalentsPane'
 
 interface BenefitUsedProps {
   label: string;
@@ -51,19 +27,24 @@ function BenefitUsed(props: BenefitUsedProps) {
   }
 }
 
-function BenefitsUsed(props: BenefitsUsedProps) {
-  const {character} = props
+// TODO: functionalize so we don't have to enumerate over every object key
+function addPowerCost(benefits: MegalosClassBenefits, power: MegalosPower): MegalosClassBenefits {
+  return ({
+    invocations: add(benefits.invocations || 0, power.costs.invocations || 0),
+    arcana: add(benefits.arcana || 0, power.costs.arcana || 0),
+    strikes: add(benefits.strikes || 0, power.costs.strikes || 0),
+    counters: add(benefits.counters || 0, power.costs.counters || 0),
+    sorceries: add(benefits.sorceries || 0, power.costs.sorceries || 0),
+    cantrips: add(benefits.cantrips || 0, power.costs.cantrips || 0),
+    talents: add(benefits.talents || 0, power.costs.talents || 0),
+  })
+}
+
+function PowersTalentsChooser() {
+  const { character, setCharacter } = useContext(CharacterContext)
 
   const benefitsUsed = reduce(
-    (benefits: MegalosClassBenefits, power: MegalosPower): MegalosClassBenefits => ({
-      invocations: add(benefits.invocations || 0, power.costs.invocations || 0),
-      arcana: add(benefits.arcana || 0, power.costs.arcana || 0),
-      strikes: add(benefits.strikes || 0, power.costs.strikes || 0),
-      counters: add(benefits.counters || 0, power.costs.counters || 0),
-      sorceries: add(benefits.sorceries || 0, power.costs.sorceries || 0),
-      cantrips: add(benefits.cantrips || 0, power.costs.cantrips || 0),
-      talents: add(benefits.talents || 0, power.costs.talents || 0),
-    }),
+    addPowerCost,
     {
       invocations: 0,
       arcana: 0,
@@ -76,65 +57,68 @@ function BenefitsUsed(props: BenefitsUsedProps) {
     character.powers
   )
 
-  return (
-    <>
-      <div className='columns'>
-        <BenefitUsed label='Invocations' value={benefitsUsed.invocations} maximum={character.class.benefits.invocations} />
-        <BenefitUsed label='Arcana' value={benefitsUsed.arcana} maximum={character.class.benefits.arcana} />
-        <BenefitUsed label='Strikes' value={benefitsUsed.strikes} maximum={character.class.benefits.strikes} />
-        <BenefitUsed label='Counters' value={benefitsUsed.counters} maximum={character.class.benefits.counters} />
-        <BenefitUsed label='Sorceries' value={benefitsUsed.sorceries} maximum={character.class.benefits.sorceries} />
-        <BenefitUsed label='Cantrips' value={benefitsUsed.cantrips} maximum={character.class.benefits.cantrips} />
-        <BenefitUsed label='Talents' value={benefitsUsed.talents} maximum={character.class.benefits.talents} />
-      </div>
-    </>
-  )
-}
-
-function PowersTalentsChooser() {
-  const { character, setCharacter } = useContext(CharacterContext)
-
-  function addPower(power: MegalosPower) {
-    setCharacter(
-      assoc(
-        "powers",
-        recalculatePowers(
-          character,
-          append(power, character.powers)
-        ),
-        character
-      )
-    )
-  }
-
-  function removePower(power: MegalosPower) {
-    setCharacter(
-      assoc(
-        "powers",
-        recalculatePowers(
-          character,
-          without([power], character.powers)
-        ),
-        character
-      )
-    )
-  }
-
   // Which powers have met prerequisites, and aren't mandatory?
   const eligiblePowers = reject((power: MegalosPower) => power.mandatory, filter(meetsPrerequisites(character), powers))
+
+  const powerSetter: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const power = find(propEq("name", event.currentTarget.value), eligiblePowers)
+    if (power) {
+      const newPowers = (event.currentTarget.checked) ? append(power, character.powers) : without([power], character.powers)
+      setCharacter(assoc("powers", recalculatePowers(character, newPowers), character))
+    }
+  }
+
+  function hasBudget(power: MegalosPower) {
+    const budgetWithPower = addPowerCost(benefitsUsed, power)
+    return (budgetWithPower.invocations || 0) <= (character.class.benefits.invocations || 0) &&
+      (budgetWithPower.arcana || 0) <= (character.class.benefits.arcana || 0) &&
+      (budgetWithPower.strikes || 0) <= (character.class.benefits.strikes || 0) &&
+      (budgetWithPower.counters || 0) <= (character.class.benefits.counters || 0) &&
+      (budgetWithPower.sorceries || 0) <= (character.class.benefits.sorceries || 0) &&
+      (budgetWithPower.cantrips || 0) <= (character.class.benefits.cantrips || 0) &&
+      (budgetWithPower.talents || 0) <= (character.class.benefits.talents || 0)
+  }
 
   return (
     <>
       <div className="columns">
         <div className="column">
-          <BenefitsUsed character={character} />
-          {map(
-            power => {
-              const isSelected = includes(power, character.powers)
-              return <EditablePowerDisplay power={power} isSelected={isSelected} togglePower={isSelected ? removePower : addPower} />
-            },
-            eligiblePowers
-          )}
+          <div className='columns'>
+            <BenefitUsed label='Invocations' value={benefitsUsed.invocations} maximum={character.class.benefits.invocations} />
+            <BenefitUsed label='Arcana' value={benefitsUsed.arcana} maximum={character.class.benefits.arcana} />
+            <BenefitUsed label='Strikes' value={benefitsUsed.strikes} maximum={character.class.benefits.strikes} />
+            <BenefitUsed label='Counters' value={benefitsUsed.counters} maximum={character.class.benefits.counters} />
+            <BenefitUsed label='Sorceries' value={benefitsUsed.sorceries} maximum={character.class.benefits.sorceries} />
+            <BenefitUsed label='Cantrips' value={benefitsUsed.cantrips} maximum={character.class.benefits.cantrips} />
+            <BenefitUsed label='Talents' value={benefitsUsed.talents} maximum={character.class.benefits.talents} />
+          </div>
+          <div className='columns is-multiline'>
+            {map(
+              power => {
+                const isSelected = includes(power, character.powers)
+                return (
+                  <>
+                    <div className='column is-full'>
+                      <label className="checkbox">
+                        <input
+                          id={`power-checkbox-${slugify(power.name.toLowerCase())}`}
+                          type="checkbox"
+                          value={power.name}
+                          disabled={
+                            !isSelected && !hasBudget(power)
+                          }
+                          checked={isSelected}
+                          onChange={powerSetter}
+                        />{' '}
+                        <strong>{power.type}</strong>: {power.name}
+                      </label>
+                    </div>
+                  </>
+                )
+              },
+              eligiblePowers
+            )}
+          </div>
         </div>
         <div className="column">
           <PowersTalentsPane />
