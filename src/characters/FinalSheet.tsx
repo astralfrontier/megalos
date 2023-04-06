4581ed7b121b562ae520e6b5c251859128bd4250
Bill Garrett <garrett@astralfrontier.org>
2023-04-06 08:41:54 -0700

    #43: preserve alignment of final sheet

--- src/characters/FinalSheet.tsx
+++ src/characters/FinalSheet.tsx
@@ -1,10 +1,10 @@
-import { compose, filter, has, map, reduce } from 'ramda'
+import { filter, has, pluck, reduce } from 'ramda'
 import React, { useContext } from 'react'
 
 import { CharacterContext } from '../GameStateProvider'
 import CharacterProfilePane from './CharacterProfilePane'
 import ClassCallingPane from './ClassCallingPane'
-import { CharacterMutator, MegalosPower } from './data'
+import { CharacterMutator } from './data'
 import PowersTalentsPane from './PowersTalentsPane'
 import SkillsPane from './SkillsPane'
 import TraitsPane from './TraitsPane'
@@ -12,8 +12,7 @@ import TraitsPane from './TraitsPane'
 function FinalSheet() {
   const { character } = useContext(CharacterContext)
 
-  const effects = map(
-    (power: MegalosPower) => power.effect,
+  const effects = pluck('effect')(
     filter(has('effect'), character.powers)
   ) as CharacterMutator[]
   const finalCharacter = reduce(
@@ -24,29 +23,15 @@ function FinalSheet() {
 
   return (
     <>
-      <div className="tile is-ancestor">
-        <div className="tile is-vertical">
-          <div className="tile">
-            <div className="tile is-parent is-vertical">
-              <div className="tile is-child">
-                <CharacterProfilePane character={finalCharacter} />
-              </div>
-              <div className="tile is-child">
-                <TraitsPane character={finalCharacter} />
-              </div>
-              <div className="tile is-child">
-                <SkillsPane character={finalCharacter} />
-              </div>
-            </div>
-            <div className="tile is-parent is-vertical">
-              <div className="tile is-child">
-                <ClassCallingPane character={finalCharacter} />
-              </div>
-              <div className="tile is-child">
-                <PowersTalentsPane character={finalCharacter} />
-              </div>
-            </div>
-          </div>
+      <div className="columns">
+        <div className="column">
+          <CharacterProfilePane character={finalCharacter} />
+          <TraitsPane character={finalCharacter} />
+          <SkillsPane character={finalCharacter} />
+        </div>
+        <div className="column">
+          <ClassCallingPane character={finalCharacter} />
+          <PowersTalentsPane character={finalCharacter} />
         </div>
       </div>
     </>
